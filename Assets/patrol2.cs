3361b929298276e69f232fd5ee7e3f4f1d7de35e
otavioscrp <otavioscrp@gmail.com>
2021-03-07 14:06:36 -0300

    Initial commit

---
+++ Assets/patrol2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol2 : MonoBehaviour
+{
+    public float speed;
+
+    public int index = 0;
+
+    public List<GameObject> alvos;
+
+
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, alvos[index].transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.CompareTag("alvo"))
+        {
+
+            next();
+
+            if (index == 0)
+            {
+                index = 3;
+            }
+
+        }
+
+    }
+
+
+    
+
+
+    public void next()
+    {
+        index++;
+        if (index >= alvos.Count)
+        {
+            index = 0;
+        }
+    }
+}
---
+++ Assets/perseguicao.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perseguicao : MonoBehaviour
+{
+
+    public GameObject alvo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, alvo.transform.position, Time.deltaTime * 10f);
+    }
+
+    
+}
